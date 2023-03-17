10b85d52178dd59a39ca68d2422565fe60fe5b11
mayrang <pkss0626@naver.com>
2023-03-18 04:37:44 +0900

    markdown 관련 작업

--- frontend/src/components/WritingBox/WritingBox.jsx
+++ frontend/src/components/WritingBox/WritingBox.jsx
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import TextareaAutosize from "react-textarea-autosize"
 import rehypeRaw from "rehype-raw";
-
+import styles from './WritingBox.module.css'
+import remarkGfm from 'remark-gfm';
+import remarkHtml from "remark-html";
 
 export default function WritingBox() {
+  const [title, setTitle] = useState("");
   const [text, setText] = useState("");
-  console.log(text)
+  
   return (
-    <div>
-      <ReactQuill theme="snow" value={text} onChange={setText}/>
-      {text}
-      <ReactMarkdown children={text} skipHtml={true} rehypePlugins={[rehypeRaw]}/>
+    <div className="flex">
+      <div className="w-1/2">
+        <div className="max-h-[540px]">
+          <div className="pt-8 pl-12 pr-12">
+            <TextareaAutosize className={styles.title} type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <div className={styles.bar}></div>
+            <div className=" flex flex-wrap">
+              <input className={styles.tag_input}/>
+            </div>
+          </div>
+        </div>
+      <textarea className={styles.editor} value={text} onChange={(e) => setText(e.target.value)} />
+
+      </div>
+    
+      {text && (
+        <ReactMarkdown 
+        className="markdown" 
+        plugins={[remarkGfm, remarkHtml]}
+        linkTarget="_blank"
+        rehypePlugins={[rehypeRaw]}
+      >
+        {text}
+      </ReactMarkdown>
+      )}
+      
     </div>
   
   );
