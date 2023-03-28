371caa825f99376f5e6d84394df41804774cd2a0
mayrang <pkss0626@naver.com>
2023-03-28 13:58:25 +0900

    qnaList pagination 적용

--- frontend/src/components/QnAList/QnAList.jsx
+++ frontend/src/components/QnAList/QnAList.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-
+import { useQuery } from "react-query";
+import axios from "axios";
+import { useRouter } from "next/router";
+import QnACard from "./../QnACard/QnACard";
+import Pagination from "./../Pagination/Pagination";
+import usePagination from "@/hooks/usePagination";
 
 export default function QnAList() {
+  const router = useRouter();
+  const { nowPage } = router.query;
+  const currentPage = nowPage ? parseInt(nowPage) : 1;
+  const {
+    data: qnaList,
+    isLoading,
+    error,
+  } = useQuery(["qnaList"], async () => {
+    const result = await axios.get("/dummy/qna.json");
+    return result.data.items;
+  });
+  const { page, pageData, pageRangeArray } = usePagination(qnaList, currentPage);
+  console.log(pageData);
   return (
-    <ul className="p-3 flex-shrink basis-0 grow max-w-[980px]">
-      {/* {qna.map((post) => (
-        <QnACard post={post} key={post.order} />
-      ))} */}
-    </ul>
+    <div className="p-3 flex-shrink basis-0 grow max-w-[980px]">
+      <ul className="mb-[30px] ">
+        {pageData.map((post) => (
+          <QnACard post={post} key={post.id} />
+        ))}
+      </ul>
+      <Pagination nowPage={currentPage} page={page} pageRangeArray={pageRangeArray} />
+    </div>
   );
 }
--- frontend/src/pages/qna.jsx
+++ frontend/src/pages/qna.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import QnAList from '@/components/QnAList/QnAList';
+import React from "react";
+import QnAList from "@/components/QnAList/QnAList";
 
 export default function qna() {
-    return (
-        <div className="flex  items-center justify-center">
+  return (
+    <div className="flex items-center justify-center">
       <QnAList />
     </div>
-    );
+  );
 }
-
