082636e6d55e86292381f9ce223449abdb1c21e0
mayrang <pkss0626@naver.com>
2023-04-02 01:07:04 +0900

    index page button -> Link

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,20 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { useRouter } from "next/router";
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter();
-  const clickPaqe = (path) => {
-    router.push(`/${path}`)
-  }
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <button className="p-3 border rounded bg-import-color text-white mr-3" onClick={() => clickPaqe("notice")}>공지사항</button>
-      <button className="p-3 border rounded bg-import-color text-white mr-3" onClick={() => clickPaqe("qna")}>큐엔에이</button>
-      <button className="p-3 border rounded bg-import-color text-white" onClick={() => clickPaqe("edit")}>글쓰기</button>
+      <Link href={"/notice"} className="p-3 border rounded bg-import-color text-white mr-3" >공지사항</Link>
+      <Link href={"/qna"} className="p-3 border rounded bg-import-color text-white mr-3" >큐엔에이</Link>
+      <Link href={"/edit"} className="p-3 border rounded bg-import-color text-white" >글쓰기</Link>
 
     </div>
   );
