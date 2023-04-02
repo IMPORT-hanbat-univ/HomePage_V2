f87ad920375809899ca51f461f632142aaade3d0
mayrang <pkss0626@naver.com>
2023-04-02 16:40:59 +0900

    development/info 페이지 생성, CardList 생성

---
+++ frontend/src/components/CardList/index.jsx
@@ -0,0 +1,23 @@
+import usePagination from '@/hooks/usePagination';
+import { useRouter } from 'next/router';
+import React from 'react';
+import Pagination from '../Pagination';
+
+export default function CardList({cardList}) {
+    const router = useRouter();
+    const { nowPage } = router.query;
+    const currentPage = nowPage ? parseInt(nowPage) : 1;
+
+    const { page, pageData, pageRangeArray } = usePagination(cardList, currentPage);
+
+    return (
+        <section>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                
+            </div>
+            <Pagination nowPage={currentPage} page={page} pageRangeArray={pageRangeArray} />
+        </section>
+       
+    );
+}
+
