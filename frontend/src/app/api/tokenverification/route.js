c14fe434fb3b72fca1aca36dafb7e48277aed6cd
mayrang <pkss0626@naver.com>
2023-05-03 10:04:31 +0900

    error notification 추가

--- frontend/src/app/api/tokenverification/route.js
+++ frontend/src/app/api/tokenverification/route.js
@@ -1,6 +1,15 @@
+import { NextResponse } from "next/server";
+
 export async function GET(req, res) {
   console.log("request", req.headers.accessToken);
 
   if (req.headers.accessToken && req.headers.refreshToken) {
+    return NextResponse.json({ success: true });
+  } else if (!req.headers.accessToken && req.headers.refreshToken) {
+    return NextResponse.json({ accessToken: "new Token", refreshToken: "new Refresh" });
+  } else if (!req.headers.accessToken && !req.headers.refreshToken) {
+    return NextResponse.json({ error: "토큰이 존재하지 않습니다." });
+  } else {
+    return NextResponse.json({ error: "유효하지 않은 토큰입니다" });
   }
 }
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -4,53 +4,72 @@ import jwt from "jsonwebtoken";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { userAtom } from "@/recoil/user";
-import axios from "axios";
+import { ReactNotifications, Store } from "react-notifications-component";
 
-export default function Home({ decodeUser }) {
+export default function Home({ decodeUser, error }) {
   const [user, setUser] = useRecoilState(userAtom);
   useEffect(() => {
     setUser(decodeUser);
   }, [decodeUser, setUser]);
 
+  useEffect(() => {
+    console.log("err", error);
+    if (error) {
+      console.log(error);
+      Store.addNotification({
+        title: "Error",
+        message: error,
+        type: "danger",
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animate__animated animate__fadeIn"], // `animate.css v4` classes
+        animationOut: ["animate__animated animate__fadeOut"],
+      });
+    }
+  }, [error]);
+
   return (
-    <div className="h-screen w-full  flex items-center justify-center flex-wrap">
-      <Link href={"/notice"} className="p-3 border rounded bg-import-color text-white mr-3">
-        공지사항
-      </Link>
-      <Link href={"/qna"} className="p-3 border rounded bg-import-color text-white mr-3">
-        QnA
-      </Link>
-      <Link href={"/qna/1"} className="p-3 border rounded bg-import-color text-white mr-3">
-        QnA 상세보기(연관게시글)
-      </Link>
-      <Link href={"/edit"} className="p-3 border rounded bg-import-color text-white mr-3">
-        글쓰기
-      </Link>
-      <Link href={"/development/info"} className="p-3 border rounded bg-import-color text-white mr-3">
-        개발 정보
-      </Link>
-      <Link href={"/development/info/1"} className="p-3 border rounded bg-import-color text-white mr-3">
-        개발 정보 상세보기
-      </Link>
-      <Link href={"/project/patchnote/1"} className="p-3 border rounded bg-import-color text-white mr-3">
-        패치노트 목록
-      </Link>
-      <Link href={"/project/patchnote/1/12"} className="p-3 border rounded bg-import-color text-white mr-3">
-        패치노트 디테일
-      </Link>
-      {user ? (
-        <>
-          <Link href={"/"} className="p-3 border rounded bg-import-color text-white">
-            로그아웃
-          </Link>
-          <div className="ml-3">{user.nick_name}</div>
-        </>
-      ) : (
-        <Link href={"http://localhost:4000/auth/kakao"} className="p-3 border rounded bg-import-color text-white">
-          로그인
+    <>
+      {error && <ReactNotifications />}
+      <div className="h-screen w-full  flex items-center justify-center flex-wrap">
+        <Link href={"/notice"} className="p-3 border rounded bg-import-color text-white mr-3">
+          공지사항
         </Link>
-      )}
-    </div>
+        <Link href={"/qna"} className="p-3 border rounded bg-import-color text-white mr-3">
+          QnA
+        </Link>
+        <Link href={"/qna/1"} className="p-3 border rounded bg-import-color text-white mr-3">
+          QnA 상세보기(연관게시글)
+        </Link>
+        <Link href={"/edit"} className="p-3 border rounded bg-import-color text-white mr-3">
+          글쓰기
+        </Link>
+        <Link href={"/development/info"} className="p-3 border rounded bg-import-color text-white mr-3">
+          개발 정보
+        </Link>
+        <Link href={"/development/info/1"} className="p-3 border rounded bg-import-color text-white mr-3">
+          개발 정보 상세보기
+        </Link>
+        <Link href={"/project/patchnote/1"} className="p-3 border rounded bg-import-color text-white mr-3">
+          패치노트 목록
+        </Link>
+        <Link href={"/project/patchnote/1/12"} className="p-3 border rounded bg-import-color text-white mr-3">
+          패치노트 디테일
+        </Link>
+        {user ? (
+          <>
+            <Link href={"/"} className="p-3 border rounded bg-import-color text-white">
+              로그아웃
+            </Link>
+            <div className="ml-3">{user.nick_name}</div>
+          </>
+        ) : (
+          <Link href={"http://localhost:4000/auth/kakao"} className="p-3 border rounded bg-import-color text-white">
+            로그인
+          </Link>
+        )}
+      </div>
+    </>
   );
 }
 
@@ -58,8 +77,8 @@ export const getServerSideProps = async ({ req, res }) => {
   fetch("http://localhost:3000/api/tokenverification", {
     method: "GET",
     headers: {
-      accessToken: "1234",
-      refreshToken: "5678",
+      accessToken: null,
+      refreshToken: null,
     },
   })
     .then((result) => {
@@ -73,31 +92,46 @@ export const getServerSideProps = async ({ req, res }) => {
   const cookieObj = parse(cookie);
   console.log(cookieObj);
   if (cookieObj?.accessToken && cookieObj?.refreshToken) {
-    // fetch("/api/user", {
-    //   method: "GET",
-    //   headers: {
-    //     accessToken: cookieObj.accessToken,
-    //     refreshToken: cookieObj.refreshToken,
-    //   },
-    // });
-
-    return {
-      props: {
-        decodeUser: {},
+    fetch("http://localhost:3000/api/tokenverification", {
+      method: "GET",
+      headers: {
+        accessToken: "1234",
+        refreshToken: "5678",
       },
-    };
-    // const user = jwt.decode(cookieObj.accessToken);
-    // console.log(user);
-    // if (user && Object.keys(user).length > 0) {
-    //   return {
-    //     props: {
-    //       decodeUser: user,
-    //     },
-    //   };
-    // }
-  } else if (!cookieObj?.accessToken && cookieObj?.refreshToken) {
+    })
+      .then((res) => {
+        console.log("res", res);
+        const user = jwt.decode(cookieObj.accessToken);
+        if (user && Object.keys(user).length > 0) {
+          return {
+            props: {
+              decodeUser: user,
+            },
+          };
+        } else {
+          return {
+            props: {},
+          };
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return {
+          props: {
+            error: "에러 발생",
+          },
+        };
+      });
+  } else if (cookieObj?.refreshToken && !cookieObj?.accessToken) {
+    fetch("http://localhost:3000/api/tokenverification", {
+      method: "GET",
+      headers: {
+        accessToken: null,
+        refreshToken: "5678",
+      },
+    });
+    return { props: {} };
+  } else {
+    return { props: { error: "에러가 발생했습니다." } };
   }
-  return {
-    props: {},
-  };
 };
