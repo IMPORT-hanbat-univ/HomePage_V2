12c317f963d09c0621d31b6027fcc3743430958c
young00000000 <qpqp01092020@gmail.com>
2023-05-27 23:43:22 +0900

    IH-114 <file upload.>

--- backend/routes/middlewares.js
+++ backend/routes/middlewares.js
@@ -182,6 +182,28 @@ exports.authenticationToken  = async (req, res, next) => {
         return res.sendStatus(404);
     }
 };
+exports.imageFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error("Only image files are allowed!"));
+    }
+    cb(null, true);
+};
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // 서버에 저장될 위치
+        cb(null, __basedir + "/app/static/assets/");
+    },
+    file: (req, file, cb) => {
+        // 서버에 저장될 때 파일 이름
+        cb(null, `${Date.now()}-bezkoder-${file.originalname}`);
+    }
+});
+
+var uploadFile = multer({ storage: storage, fileFilter: imageFilter }).single(
+    // 프론트에서 넘겨울 params key 값, 오른쪽 같이 넘겨줘야함-> {photo: binary}
+    "photo"
+);
 
 //
 
