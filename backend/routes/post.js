21e72ca29dcb8b2bc68dc4d11af3164b0df2c3cf
young00000000 <qpqp01092020@gmail.com>
2023-06-09 22:22:48 +0900

    IH-125 <data findAll 공통화>

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -7,49 +7,69 @@ const { v4: uuidv4 } = require("uuid");
 const { Op } = require("sequelize");
 // Helper function to get table and tableComment based on tableCategory
 const getTables = (tableCategory) => {
-    let table, tableComment, tableId;
+    let table, tableComment, relatedTableId;
 
     switch (tableCategory) {
         case 'notice':
             table = RootPost;
             tableComment = RootComment;
-            tableId = "RootPostId";
+            relatedTableId = "RootPostId";
             break;
         case 'qna':
             table = ListPost;
             tableComment = ListPostComment;
-            tableId = "ListPostId";
+            relatedTableId = "ListPostId";
             break;
         case 'information':
             table = CardPost;
             tableComment = CardPostComment;
-            tableId = "CardPostId";
+            relatedTableId = "CardPostId";
             break;
         case 'project':
             table = Project;
             tableComment = ProjectComment;
-            tableId = "ProjectId";
+            relatedTableId = "ProjectId";
             break;
         case 'patch':
             table = PatchNote;
             tableComment = PatchNoteComment;
-            tableId = "PatchNoteId";
+            relatedTableId = "PatchNoteId";
             break;
         default:
             throw new Error('테이블을 불러오지 못했습니다.');
     }
 
-    return { table, tableComment, tableId };
+    return { table, tableComment, relatedTableId };
 };
+const getdata = async (table,column, dataId) =>{
+    const data = await table.findAll({
+        where: {
+            [column]:dataId ,
+        },
+        raw: true,
+        include: [ //조인
+            {
+                model: User,
+                attributes: ["nick_name", "rank"],
+            },
+        ],
+    });
+    data.forEach((obj) => {
+        obj.rank = obj["User.rank"];
+        obj.nick_name = obj["User.nick_name"];
+        delete obj["User.rank"];
+        delete obj["User.nick_name"];
+    });
+    return data;
 
+};
 
 //목록
 router.get("/", async function (req, res) {
     const body = req.body;
-    const user = req.user;
     body.tableCategory = "notice";
     try {
-        const { table, tableComment, tableId } = getTables(body.tableCategory);
+        const { table, tableComment, relatedTableId } = getTables(body.tableCategory);
 
         const posts = await table.findAll({
             raw: true,
@@ -80,6 +100,7 @@ router.post("/edit",verifyToken, async (req, res) => {
     const body = req.body;
     const user = req.user;
     body.tableCategory = "notice";
+    let table, postId;
 
     /*
     1. 큰 카테고리(카테고리-테이블명 / notice-RootPost, QnA-ListPost, information(개발정보)-CardPost,project-Project, patch-PatchNote)
@@ -161,24 +182,8 @@ router.post("/edit",verifyToken, async (req, res) => {
             default:
                 throw new Error('데이터를 저장하지 못했습니다.');
         }
-        const nowPost = await table.findAll({
-            where: {
-                id: { [Op.eq]: postId },
-            },
-            raw: true,
-            include: [//조인
-                {
-                    model: User, //만약에 rootPost가 root랑 연결되면 여기부분 바꿔야함
-                    attributes: ["nick_name", "rank"],
-                },
-            ],
-        });
-        nowPost.forEach((obj) => {
-            obj.rank = obj["User.rank"];
-            obj.nick_name = obj["User.nick_name"];
-            delete obj["User.rank"];
-            delete obj["User.nick_name"];
-        });
+        const nowPost =await getdata(table,"id", postId);
+
         console.log({ content: nowPost[0] });
         return res.json({ content: nowPost[0] });
 
@@ -193,52 +198,17 @@ router.post("/edit",verifyToken, async (req, res) => {
 //상세조회
 router.get("/:id",async (req, res) => {
     const body = req.body;
-    const user = req.user;
+
     body.tableCategory = "notice";
     try {
-    const { table, tableComment, tableId } = getTables(body.tableCategory);
-        const post = await table.findAll({
-            attributes: ["id", "title", "content", "tagF", "tagS", "tagT", "category", "file", "createdAt", "updatedAt", "deletedAt", "UserId"],
-            where: {
-                id: { [Op.eq]: req.params.id },
-            },
-            raw: true,
-            include: [ //조인
-                {
-                    model: User,
-                    attributes: ["nick_name", "rank"],
-                },
-            ],
-        });
-        post.forEach((obj) => {
-            obj.rank = obj["User.rank"];
-            obj.nick_name = obj["User.nick_name"];
-            delete obj["User.rank"];
-            delete obj["User.nick_name"];
-        });
-
+        const { table, tableComment, relatedTableId } = getTables(body.tableCategory);
+        const post = await getdata(table,"id",req.params.id);
         if (!post) {
             return res.status(404).send("Post not found");
         }
         //댓글
-        const comments = await tableComment.findAll({
-            raw:true,
-            where: {
-                [tableId]: { [Op.eq]: req.params.id },
-            },
-            include: [//조인
-                {
-                    model: User,
-                    attributes: ["nick_name", "rank"],
-                },
-            ],
-        })
-        comments.forEach((obj) => {
-            obj.rank = obj["User.rank"];
-            obj.nick_name = obj["User.nick_name"];
-            delete obj["User.rank"];
-            delete obj["User.nick_name"];
-        });
+        const comments = await getdata(tableComment,relatedTableId,req.params.id);
+
         console.log({ content: post[0],comment:comments });
         res.json({ content: post[0] ,comment:comments});
     } catch (error) {
@@ -250,20 +220,21 @@ router.get("/:id",async (req, res) => {
 router.post("/edit/:id", verifyToken,async (req, res) => {
     const body = req.body;
     const user = req.user;
+    let table, postId;
     body.tableCategory = "notice";
     try {
         switch (body.tableCategory){
             case 'notice':
                 const notice = await RootPost.update({
-                    title: body.title,
-                    content: body.content,
-                    tagF: body.tagF,
-                    tagS: body.tagS,
-                    tagT: body.tagT,
-                    category: body.category,
-                    file: "",
-                    UserId: user.userId,
-                },
+                        title: body.title,
+                        content: body.content,
+                        tagF: body.tagF,
+                        tagS: body.tagS,
+                        tagT: body.tagT,
+                        category: body.category,
+                        file: "",
+                        UserId: user.userId,
+                    },
                     {
                         where: {
                             id: { [Op.eq]: req.params.id },
@@ -274,15 +245,15 @@ router.post("/edit/:id", verifyToken,async (req, res) => {
                 break
             case 'qna':
                 const qna = await ListPost.update({
-                    title: body.title,
-                    content: body.content,
-                    tagF: body.tagF,
-                    tagS: body.tagS,
-                    tagT: body.tagT,
-                    category: body.category,
-                    file: "",
-                    UserId: user.userId,
-                },
+                        title: body.title,
+                        content: body.content,
+                        tagF: body.tagF,
+                        tagS: body.tagS,
+                        tagT: body.tagT,
+                        category: body.category,
+                        file: "",
+                        UserId: user.userId,
+                    },
                     {
                         where: {
                             id: { [Op.eq]: req.params.id },
@@ -308,17 +279,17 @@ router.post("/edit/:id", verifyToken,async (req, res) => {
             case 'project':
 
                 const project = await Project.update({
-                    title: body.title,
-                    content: body.content,
-                    tagF: body.tagF,
-                    tagS: body.tagS,
-                    tagT: body.tagT,
-                    category: body.category,
-                    file: "",
-                    leader:user.userId,
-                    member:body.member,
-                    UserId: user.userId,
-                },
+                        title: body.title,
+                        content: body.content,
+                        tagF: body.tagF,
+                        tagS: body.tagS,
+                        tagT: body.tagT,
+                        category: body.category,
+                        file: "",
+                        leader:user.userId,
+                        member:body.member,
+                        UserId: user.userId,
+                    },
                     {
                         where: {
                             id: { [Op.eq]: req.params.id },
@@ -329,12 +300,12 @@ router.post("/edit/:id", verifyToken,async (req, res) => {
                 break
             case 'patch':
                 const patch = await PatchNote.update({
-                    title: body.title,
-                    content: body.content,
-                    category: body.category,
-                    file: "",
-                    UserId: user.userId,
-                },
+                        title: body.title,
+                        content: body.content,
+                        category: body.category,
+                        file: "",
+                        UserId: user.userId,
+                    },
                     {
                         where: {
                             id: { [Op.eq]: req.params.id },
@@ -346,25 +317,8 @@ router.post("/edit/:id", verifyToken,async (req, res) => {
             default:
                 throw new Error('데이터를 수정하지 못했습니다.');
         }
+        const updatedPost = await getdata(table, "id", req.params.id);
 
-        const updatedPost = await RootPost.findAll({
-             where: {
-                id: { [Op.eq]: req.params.id },
-            },
-            raw: true,
-            include: [ //조인
-                {
-                    model: User,
-                    attributes: ["nick_name", "rank"],
-                },
-            ],
-        });
-        updatedPost.forEach((obj) => {
-            obj.rank = obj["User.rank"];
-            obj.nick_name = obj["User.nick_name"];
-            delete obj["User.rank"];
-            delete obj["User.nick_name"];
-        });
         console.log({ content: updatedPost[0] });
         return res.json({ content: updatedPost[0] });
     } catch (error) {
@@ -374,12 +328,12 @@ router.post("/edit/:id", verifyToken,async (req, res) => {
 });
 
 //글 삭제
-router.delete("/:id", async (req, res) => {
+router.delete("/deleted/:id", async (req, res) => {
     const body = req.body;
     //const user = req.user;
     body.tableCategory = "notice";
     try{
-        const { table, tableComment, tableId } = getTables(body.tableCategory);
+        const { table, tableComment, relatedTableId } = getTables(body.tableCategory);
         await table.destroy({
             where: {
                 id: { [Op.eq]: req.params.id },
@@ -401,13 +355,13 @@ router.post("/comment/:id", verifyToken,async (req, res) => {
     const user = req.user;
     body.tableCategory = "notice";
     try {
-        const { table, tableComment, tableId } = getTables(body.tableCategory);
+        const { table, tableComment, relatedTableId } = getTables(body.tableCategory);
 
         const result = await tableComment.findOne({
             attributes: [[sequelize.fn("MAX", sequelize.cast(sequelize.col("sequence"), "INTEGER")), "max_sequence"]],
             where: {
                 group: body.group,
-                [tableId]: req.params.id ,
+                [relatedTableId]: req.params.id ,
             },
         });
 
@@ -417,35 +371,17 @@ router.post("/comment/:id", verifyToken,async (req, res) => {
 
         console.log("가장 큰 숫자:", maxSequence);
 
-        const newcomment = await tableComment.create({
+        const newComment = await tableComment.create({
             content: body.content,
             group: body.group,
             sequence: sequence,
             UserId: user.userId,
-            [tableId]: req.params.id,
+            [relatedTableId]: req.params.id,
         });
+        const nowComment= await getdata(tableComment,relatedTableId,newComment.id);
 
-        const nowcomment = await tableComment.findAll({
-            attributes: ["content", "sequence", "group", "createdAt", "UserId"],
-            where: {
-                id: { [Op.eq]: newcomment.id },
-            },
-            raw: true,
-            include: [
-                {
-                    model: User,
-                    attributes: ["nick_name", "rank"],
-                },
-            ],
-        });
-        nowcomment.forEach((obj) => {
-            obj.rank = obj["User.rank"];
-            obj.nick_name = obj["User.nick_name"];
-            delete obj["User.rank"];
-            delete obj["User.nick_name"];
-        });
-        console.log({ content: nowcomment[0] });
-        return res.json({ content: nowcomment[0] });
+        console.log({ content: nowComment[0] });
+        return res.json({ content: nowComment[0] });
     } catch (error) {
         console.error(error);
         return res.sendStatus(401);
@@ -459,40 +395,22 @@ router.post("/comment/:id/:commentId", verifyToken,async (req, res) => {
     const user = req.user;
     body.tableCategory = "notice";
     try {
-        const { table, tableComment, tableId } = getTables(body.tableCategory);
+        const { table, tableComment, relatedTableId } = getTables(body.tableCategory);
 
-        const newcomment = await tableComment.update({
-            content: body.content,
-            UserId: user.userId,
-            [tableId]: req.params.id,
-        },
+        await tableComment.update({
+                content: body.content,
+                UserId: user.userId,
+                [relatedTableId]: req.params.id,
+            },
             {
                 where: {
                     id: { [Op.eq]: req.params.commentId },
                 },
             });
 
-        const nowcomment = await tableComment.findAll({
-            attributes: ["content", "sequence", "group", "createdAt", "UserId"],
-            where: {
-                id: { [Op.eq]: newcomment.id },
-            },
-            raw: true,
-            include: [
-                {
-                    model: User,
-                    attributes: ["nick_name", "rank"],
-                },
-            ],
-        });
-        nowcomment.forEach((obj) => {
-            obj.rank = obj["User.rank"];
-            obj.nick_name = obj["User.nick_name"];
-            delete obj["User.rank"];
-            delete obj["User.nick_name"];
-        });
-        console.log({ content: nowcomment[0] });
-        return res.json({ content: nowcomment[0] });
+        const nowComment= await getdata(tableComment,"id",req.params.commentId);
+        console.log({ content: nowComment[0] });
+        return res.json({ content: nowComment[0] });
     } catch (error) {
         console.error(error);
         return res.sendStatus(401);
@@ -500,12 +418,12 @@ router.post("/comment/:id/:commentId", verifyToken,async (req, res) => {
 });
 
 //댓글 삭제
-router.delete("/:id/:commentId",verifyToken, async (req, res) => {
+router.delete("/deleted/:id/:commentId",verifyToken, async (req, res) => {
     const body = req.body;
     const user = req.user;
     body.tableCategory = "notice";
     try{
-        const { table, tableComment, tableId } = getTables(body.tableCategory);
+        const { table, tableComment, relatedTableId } = getTables(body.tableCategory);
         await tableComment.destroy({
             where: {
                 id: { [Op.eq]: req.params.commentId },
@@ -528,7 +446,7 @@ router.post('/file',upload.single('fileupload'),verifyToken,function (req,res){
     console.log(upload)
     console.log(upload.storage.getFilename)
 
-    return res.send(req.file.path)
+    //return res.send(req.file.path)
 
     res.redirect('/');
 })
