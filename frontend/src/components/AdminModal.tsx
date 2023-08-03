a08fa0435422c7f92771c4f13970e65b30ca0d83
mayrang <pkss0626@naver.com>
2023-08-04 00:37:28 +0900

    user admin modal 수정 디자인 완성

--- frontend/src/components/AdminModal.tsx
+++ frontend/src/components/AdminModal.tsx
@@ -1,5 +1,6 @@
+"use client";
 import { DetailUser } from "@/util/type";
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import Image from "next/image";
 type Props = {
   data: DetailUser;
@@ -15,10 +16,20 @@ const rank_array = [
 ];
 
 export default function AdminModal({ data }: Props) {
+  const [modifyData, setModifyData] = useState(data);
+  const [isModify, setIsModify] = useState(false);
   console.log("modal", data);
   const { nick_name, createdAt, email, rank, blog, department, framework, github_url, grade, language, profileImg } =
     data;
   const rank_title = rank_array.find((item) => item.value === rank.toString())?.title;
+  const changeInput = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setModifyData({ ...modifyData, [e.target.name]: e.target.value });
+  };
+  const submitModify = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(modifyData);
+    setIsModify(false);
+  };
   return (
     <article className="p-12 flex flex-col items-center h-full justify-around">
       <div className="text-xl">
@@ -30,71 +41,175 @@ export default function AdminModal({ data }: Props) {
             height={200}
             src={profileImg ? `http://localhost:4000${profileImg}` : "/images/import_image.jpg"}
           />
-          <div>
-            <div>
+          <div className="flex flex-col gap-1 text-lg">
+            <div className="flex items-center w-full">
               <label htmlFor="nick_name" className="mr-2">
                 닉네임:
               </label>
-              <span id="nick_name">{nick_name}</span>
+              {isModify ? (
+                <input
+                  name="nick_name"
+                  id="nick_name"
+                  className="border px-1 grow border-black rounded"
+                  value={modifyData.nick_name}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="nick_name">{nick_name}</span>
+              )}
             </div>
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="email" className="mr-2">
                 Email:
               </label>
-              <span id="email">{email}</span>
+              {isModify ? (
+                <input
+                  name="email"
+                  id="email"
+                  className="border  px-1 grow  border-black rounded"
+                  value={modifyData.email}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="email">{email}</span>
+              )}
             </div>
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="email" className="mr-2">
                 Rank:
               </label>
-              <span id="email">{rank_title ? rank_title : "랭크를 찾을 수 없음"}</span>
+              {isModify ? (
+                <select
+                  className="border grow border-black"
+                  value={modifyData.rank}
+                  onChange={changeInput}
+                  id="rank"
+                  name="rank"
+                >
+                  {rank_array.map(({ value, title }) => (
+                    <option key={value} value={value}>
+                      {title}
+                    </option>
+                  ))}
+                </select>
+              ) : (
+                <span id="rank">{rank_title ? rank_title : "랭크를 찾을 수 없음"}</span>
+              )}
             </div>
           </div>
         </div>
 
         {parseInt(rank) > 1 && (
           <div className="flex flex-col gap-3">
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="department" className="mr-2">
                 학과:
               </label>
-              <span id="department">{department}</span>
+              {isModify ? (
+                <input
+                  name="department"
+                  id="department"
+                  className="border px-1 grow border-black rounded"
+                  value={modifyData.department}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="department">{department}</span>
+              )}
             </div>
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="grade" className="mr-2">
                 학년:
               </label>
-              <span id="grade">{grade}</span>
+              {isModify ? (
+                <input
+                  name="grade"
+                  id="grade"
+                  className="border grow px-1 border-black rounded"
+                  value={modifyData.grade}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="grade">{grade}</span>
+              )}
             </div>
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="language" className="mr-2">
                 언어:
               </label>
-              <span id="language">{language}</span>
+              {isModify ? (
+                <input
+                  name="language"
+                  id="language"
+                  className="border px-1 grow border-black rounded"
+                  value={modifyData.language}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="language">{language}</span>
+              )}
             </div>
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="framework" className="mr-2">
                 프레임워크:
               </label>
-              <span id="framework">{framework}</span>
+              {isModify ? (
+                <input
+                  name="framework"
+                  id="framework"
+                  className="border px-1 grow border-black rounded"
+                  value={modifyData.framework}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="framework">{framework}</span>
+              )}
             </div>
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="blog" className="mr-2">
                 블로그:
               </label>
-              <span id="blog">{blog}</span>
+              {isModify ? (
+                <input
+                  name="blog"
+                  id="blog"
+                  className="border px-1 grow border-black rounded"
+                  value={modifyData.blog}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="blog">{blog}</span>
+              )}
             </div>
-            <div>
+            <div className="flex items-center w-full">
               <label htmlFor="github_url" className="mr-2">
                 깃허브:
               </label>
-              <span id="github_url">{github_url}</span>
+              {isModify ? (
+                <input
+                  name="github_url"
+                  id="github_url"
+                  className="border px-1 grow border-black rounded"
+                  value={modifyData.github_url}
+                  onChange={changeInput}
+                />
+              ) : (
+                <span id="github_url">{github_url}</span>
+              )}
             </div>
           </div>
         )}
       </div>
       <div className="w-full flex justify-end ">
-        <button className="px-3 py-2 bg-import-color rounded border-none">수정</button>
+        {isModify ? (
+          <button onClick={submitModify} className="px-3 py-2 bg-import-color rounded border-none">
+            저장
+          </button>
+        ) : (
+          <button onClick={() => setIsModify(true)} className="px-3 py-2 bg-import-color rounded border-none">
+            수정
+          </button>
+        )}
       </div>
     </article>
   );
