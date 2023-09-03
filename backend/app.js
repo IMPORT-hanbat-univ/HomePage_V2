7a3c7a76c6438c8e34269ed416552b59b8f02aa5
young00000000 <qpqp01092020@gmail.com>
2023-09-03 23:59:55 +0900

    약간 수정

--- backend/app.js
+++ backend/app.js
@@ -93,9 +93,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(4006, function() {
-  console.log("start! express server on port 4000")
-})
+
 
 
 module.exports = app;
