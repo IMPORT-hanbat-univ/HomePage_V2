fa8c93e9cb44b59ad9a5aa57b5d4c716ce9d104b
mayrang <pkss0626@naver.com>
2023-09-10 02:22:19 +0900

    main section 수정

--- frontend/src/components/ui/MainSection.tsx
+++ frontend/src/components/ui/MainSection.tsx
@@ -8,45 +8,45 @@ export default function MainSection() {
     <section className="w-full h-[calc(100vh-60px)] bg-black">
       <div className="w-full relative h-full flex justify-center">
         <div className="w-full relative h-[63vh]  lg:h-[485px]  bg-black overflow-hidden ">
-          <div className="md:w-[1960px] w-[800px]  absolute md:-top-36 -left-4 h-full rotate-[-15deg]">
+          <div className="md:w-[2000px] w-[800px]  absolute md:-top-36 -left-5 h-full rotate-[-15deg]">
             <MarqueeContainer style={"-left-12"}>
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/git.png"
                 alt="git"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/react.png"
                 alt="react"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/npm.png"
                 alt="npm"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/javascript.png"
                 alt="javascript"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/css.png"
                 alt="css"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/vscode.png"
                 alt="vscode"
                 width={218}
@@ -55,21 +55,21 @@ export default function MainSection() {
             </MarqueeContainer>
             <MarqueeContainer style={" -left-12"}>
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/vscode.png"
                 alt="vscode"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/flutter.png"
                 alt="flutter"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/vue.png"
                 alt="vue"
                 width={218}
@@ -77,21 +77,21 @@ export default function MainSection() {
               />
 
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/nest.png"
                 alt="nest"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/java.png"
                 alt="java"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/mongodb.png"
                 alt="mongodb"
                 width={218}
@@ -100,28 +100,28 @@ export default function MainSection() {
             </MarqueeContainer>
             <MarqueeContainer style={"-left-12"}>
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/javascript.png"
                 alt="javascript"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/git.png"
                 alt="git"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/nest.png"
                 alt="nest"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/css.png"
                 alt="css"
                 width={218}
@@ -129,14 +129,14 @@ export default function MainSection() {
               />
 
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/vue.png"
                 alt="vue"
                 width={218}
                 height={218}
               />
               <Image
-                className="md:mx-[55px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
+                className="md:mx-[58px] mx-[30px] w-[150px] md:w-[218px] h-[150px] md:h-[218px]"
                 src="/images/npm.png"
                 alt="npm"
                 width={218}
@@ -145,7 +145,7 @@ export default function MainSection() {
             </MarqueeContainer>
           </div>
         </div>
-        <div className="absolute bottom-16 pt-5 w-full bg-black md:bottom-28 text-center text-2xl md:text-[50px] font-extrabold md:tracking-[-0.75px] md:leading-[60px]">
+        <div className="absolute bottom-16 pt-5 w-full bg-black md:bottom-20 text-center text-2xl md:text-[50px] font-extrabold md:tracking-[-0.75px] md:leading-[60px]">
           <div className="text-import-color ">
             <p>IMPORT와 함께 경험하고 성장할</p>
             <p>여러분들을 기다리고 있습니다.</p>
